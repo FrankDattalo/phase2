57f3eacaad6a1bf3ee560a6b20609704f3655980
Frank Dattalo <dattalo.2@osu.edu>
2017-04-09 21:26:56 -0400

    added descriptions for queries 4 - 6

--- query.sql
+++ query.sql
@@ -3,3 +3,9 @@
 -- Query 2: Select names of customers who have both reserved a room for an event and for stay
 
 -- Query 3: Select name and number of branch and amount of people currently staying at that branch
+
+-- Query 4: Names of customers who have attended a specific event
+
+-- Query 5: Retrieve the name and number of branch and amount and the total food cost for the past 30 days
+
+-- Query 6: Retrieve the ssn and name of workers managed by a specific corporate office
