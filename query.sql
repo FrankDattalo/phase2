54d281db0bc16ced8dbca4759e9aa13039bfdc80
Frank Dattalo <dattalo.2@osu.edu>
2017-04-09 21:18:51 -0400

    added descriptions for queries 1 - 3

---
+++ query.sql
@@ -0,0 +1,5 @@
+-- Query 1: Retrieve information of customers who have been billed in the last week
+
+-- Query 2: Select names of customers who have both reserved a room for an event and for stay
+
+-- Query 3: Select name and number of branch and amount of people currently staying at that branch
